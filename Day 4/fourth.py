166eec65f078912608091c80c1a12de111903cfc
Mohammad Danish <thatsdanish02@gmail.com>
2024-01-11 19:49:59 +0530

    day 4

---
+++ Day 4/fourth.py
@@ -0,0 +1,19 @@
+# String slicing
+original_string = "Hello, CWH!"
+sliced_string = original_string[7:12]  # Slicing from index 7 to 11
+print("Original String:", original_string)
+print("Sliced String:", sliced_string)
+
+# String functions
+uppercase_string = original_string.upper()
+lowercase_string = original_string.lower()
+length_of_string = len(original_string)
+concatenated_strings = original_string + " Welcome!"
+replaced_string = original_string.replace("CWH", "LEARNATHON MEMBERS")
+
+# Displaying results
+print("Uppercase String:", uppercase_string)
+print("Lowercase String:", lowercase_string)
+print("Length of String:", length_of_string)
+print("Concatenated Strings:", concatenated_strings)
+print("Replaced String:", replaced_string)
